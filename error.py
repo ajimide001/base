5d9a79e51d1c4efc42207d42d8d2409a932fa4cd
ajimide001 <klmnopq001>
2018-02-01 18:28:40 +0800

    RedisClient

---
+++ error.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+class CookiePoolError(Exception):
+    def __str__(self):
+        return repr('Cookie Pool Error')
+
+
+class SetCookieError(CookiePoolError):
+    def __str__(self):
+        return repr('Set Cookie Error')
+
+
+class GetCookieError(CookiePoolError):
+    def __str__(self):
+        return repr('Get Cookie Error')
+
+
+class GetRandomCookieError(CookiePoolError):
+    def __str__(self):
+        return repr('Get Random Cookie Error')
+
+
+class GetAllCookieError(CookiePoolError):
+    def __str__(self):
+        return repr('Get All Cookie Error')
+
+
+class DeleteCookieError(CookiePoolError):
+    def __str__(self):
+        return repr('Delete Cookie Error')
+
+
+class SetAccountError(CookiePoolError):
+    def __str__(self):
+        return repr('Set Account Error')
+
+
+class GetAllAccountError(CookiePoolError):
+    def __str__(self):
+        return repr('Get All Account Error')
+
+
+class DeleteAccountError(CookiePoolError):
+    def __str__(self):
+        return repr('Delete Account Error')
--- redis_db.py
+++ redis_db.py
@@ -2,7 +2,12 @@
 # -*- coding:utf-8 -*-
 import random
 import redis
-
+from error import *
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = '6379'
+REDIS_PASSWORD = ''
+REDIS_DOMAIN = ''
+REDIS_NAME = ''
 class RedisClient(object):
     def __init__(self,host=REDIS_HOST, port=REDIS_PORT,password=REDIS_PASSWORD,db=0):
         """
@@ -12,3 +17,45 @@ class RedisClient(object):
         :param password:密码
         :param db:
         """
+        if password:
+            self._db = redis.Redis(host=host, port=port, password=password,db=db)
+        else:
+            self_db = redis.Redis(host=host,port=port)
+        self.domain = REDIS_DOMAIN
+        self.name = REDIS_NAME
+
+    def _key(self, key):
+        """
+        得到格式化的key
+        :param key: 最后一个参数key
+        :return:
+        """
+        return "{domain}:{name}:{key}".format(domain=self.domain, name=self.name, key=key)
+
+    def set(self,key,value):
+        """
+        设置键值对
+        :param key:
+        :param value:
+        :return:
+        """
+        raise NotImplementedError
+    def delete(self,key):
+        """
+        根据键名删除键值对
+        :param key:
+        :return:
+        """
+        raise NotImplementedError
+    def keys(self):
+        """
+        得到所有的键名
+        :return:
+        """
+        return self._db.keys('{domian}:{name}:*'.format(domian=self.domain, name=self,name))
+    def flush(self):
+        """
+        清空数据库。慎用
+        :return:
+        """
+        self._db.flushall()
